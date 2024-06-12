62ad4453e9a7ec1bb230be80486caef6fa946223
drowninginseaofbooks <153051064+drowninginseaofbooks@users.noreply.github.com>
2024-06-12 10:33:30 +0545

    new commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("Hello World");
 
 // Assigning value to the variables
 let x = 0;
-const y = 0;
+let y = 0;
 
 
 // Three Data Types in Js: Numbers, String and Boolean
@@ -23,3 +23,26 @@ console.log("Division: ", a/b);
 // Typeof shows the type of a variable data
 let z = true;
 console.log(typeof z);
+
+function testFunc(){
+    console.log("Hello World");
+}
+
+function addFunc(num1, num2){
+    console.log("Addition: ", num1+num2);
+}
+
+function subFunc(num1, num2){
+    console.log("Subtraction: ", num1-num2);
+}
+
+
+function passStringPara(){   
+    let yourName = document.getElementById("yourName").value;
+    console.log("Your Name: ", yourName);
+    changeText(yourName);
+}
+
+function changeText(){
+    h1.innerHTML = "Welcome Back "+ yourName.value;
+}
