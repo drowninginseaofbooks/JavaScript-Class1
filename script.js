995776a13c071e460f89ec9ca435551e40eece01
drowninginseaofbooks <153051064+drowninginseaofbooks@users.noreply.github.com>
2024-06-12 10:03:29 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Printing on Terminal
+console.log("Hello World");
+
+
+// Assigning value to the variables
+let x = 0;
+const y = 0;
+
+
+// Three Data Types in Js: Numbers, String and Boolean
+x = 10;
+
+console.log(x/y);
+
+// Calculating Two Numbers
+let a = 10;
+let b = 10;
+console.log("Addition: ", a+b);
+console.log("Subtraction: ", a-b);
+console.log("Multiplication: ", a*b);
+console.log("Division: ", a/b);
+
+// Typeof shows the type of a variable data
+let z = true;
+console.log(typeof z);
